6b3e1a27d926ac22865ed4351e36041beb0b6b2b
Steven Carlson <sncarlson@dev-hive.org>
2020-09-12 13:25:11 -0400

    Initial commit

---
+++ src/main/java/org/devhive/sensors/SensorsApplication.java
@@ -0,0 +1,13 @@
+package org.devhive.sensors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/devhive/sensors/SensorsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devhive.sensors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
